40c0228a0955f7accc7891bcadade3e89a6f7155
Antara Kundu <61645954+antarakundu08@users.noreply.github.com>
2024-03-14 15:38:16 +0530

    added hooks and shimmer ui

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,44 @@
+import { useEffect, useState } from "react";
 import { SEARCH_URL } from "../utils/constants";
-import restList from "../utils/mockData";
 import RestCard from "./RestCard";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
+    const [listOfRestaurants, setListOfRestaurants] = useState([]);
+    useEffect(() => {
+        fetchData();    
+    },[])
+
+const fetchData = async () => {
+    const response = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.07480&lng=72.88560&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+    const json = await response.json()
+    // console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+    setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+}
+
+//Conditonal Rendering
+
+    if (listOfRestaurants.length === 0) {
+        return <div>
+            <div className="search">
+            <input type="text" className='searchbox' placeholder='Search....' />
+            <img src={SEARCH_URL}
+             alt="search" className="searchlogo" />
+        </div>
+        <div className="filter">
+            <button className="filter-btn" onClick={
+                () => {
+                    const myList = listOfRestaurants.filter(
+                        (res) => res.info.avgRating > 4.0
+                    );
+                    setListOfRestaurants(myList)                  
+                }
+            }>
+                Top Rated Restaurants</button>
+        </div>
+        < Shimmer />
+        </div>
+    }
     return (
     <div>
         <div className="search">
@@ -10,9 +46,20 @@ const Body = () => {
             <img src={SEARCH_URL}
              alt="search" className="searchlogo" />
         </div>
+        <div className="filter">
+            <button className="filter-btn" onClick={
+                () => {
+                    const myList = listOfRestaurants.filter(
+                        (res) => res.info.avgRating > 4.0
+                    );
+                    setListOfRestaurants(myList)                  
+                }
+            }>
+                Top Rated Restaurants</button>
+        </div>
         <div className="card-sec">
             {
-                restList.map((restaurant) => (
+                listOfRestaurants.map((restaurant) => (
                     <RestCard key = {restaurant.info.id} resObj = {restaurant} />
                 ))
             }
